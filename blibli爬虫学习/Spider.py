4a370ff276e224a500bca669aee9e3e7b5a47581
Svanur <53872146+SvanurH@users.noreply.github.com>
2020-12-30 12:12:09 +0800

    Add files via upload

--- blibli爬虫学习/Spider.py
+++ blibli爬虫学习/Spider.py
@@ -8,13 +8,15 @@ class SpiderForView:
 
     def run(self):
         for i in range(1, 2):
-            data_video = self.__spider(self.video_url.format(page=i))
-            if (data_video['data']['list']):
-                for info in self.__video_data_handle(data_video['data']['list']):
-                    self.__spider_tags()
-                    print(info)
-            else:
-                break
+            self.__run_spider(i)
+
+    def __run_spider(self, i):
+        data_video = self.__spider(self.video_url.format(page=i))
+        if data_video['data']['list']:
+            for info in self.__video_data_handle(data_video['data']['list']):
+                tags = self.__spider_tags(info['video_info']['aid'])
+                info['video_info']['tags'] = tags
+                yield info
 
     def __spider(self, url):
         res = requests.get(url).json()
@@ -59,14 +61,17 @@ class SpiderForView:
             yield data_video
 
     def __spider_tags(self, aid):
-        tag_api_url = 'https://api.bilibili.com/x/web-interface/view/detail/tag?aid='+str(aid)
+        tag_api_url = 'https://api.bilibili.com/x/web-interface/view/detail/tag?aid=' + str(aid)
         tag_list = self.__spider(tag_api_url)['data']
         tags = []
         for tag in tag_list:
             tags.append(tag)
         return tags
 
+    def __storage(self):
+        pass
 
 a = SpiderForView()
 a.run()
+
 '''待实现内容，获取每个视频的标签，并加入到视频当中'''
